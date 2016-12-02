2c3a8cc4e4b29ba80e6384bcb8854dd1fa87fe0e
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-12-02 22:05:29 +0100

    rails generate model ingredient name:string meal:belongs_to --no-assets --no-jbuilder --no-helper --no-test-framework

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202205616) do
+ActiveRecord::Schema.define(version: 20161202210419) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "meal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meal_id"], name: "index_ingredients_on_meal_id"
+  end
 
   create_table "meals", force: :cascade do |t|
     t.datetime "consumption_time"
